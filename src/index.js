12ec9875c7e7507bf39a0c500ee1eb743a76bbdc
edoubart <edouard.barthelemy@gmail.com>
2025-02-20 06:04:23 -0500

    Get Wallet Balance.

--- src/index.js
+++ src/index.js
@@ -12,5 +12,24 @@ const wallet = new Keypair();
 const publicKey = new PublicKey(wallet._keypair.publicKey);
 const secretKey = wallet._keypair.secretKey;
 
-console.log('publicKey: ', publicKey);
-console.log('secretKey: ', secretKey);
+//console.log('publicKey: ', publicKey);
+//console.log('secretKey: ', secretKey);
+
+async function getWalletBalance() {
+  try {
+    let connection = new Connection(clusterApiUrl('devnet', 'confirmed'));
+
+    let walletBalance = await connection.getBalance(publicKey);
+
+    console.log('walletBalance: ', walletBalance);
+  }
+  catch(err) {
+    console.error("Something went wrong getting wallet balance: ", err);
+  }
+}
+
+async function main() {
+  await getWalletBalance();
+}
+
+main();
